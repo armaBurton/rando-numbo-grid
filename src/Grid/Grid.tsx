bb83119b95582bfab767a0cde5a1133f94a67725
armaBurton <armanarma@armaburton.com>
2022-11-14 17:53:25 -0800

    new branch starting grid

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,3 +1,16 @@
 export const Grid = () => {
-  return <section className="">farts</section>;
+  const randoNumbo = () => {
+    const randoNumbo = Math.ceil(Math.random() * 9);
+    return <>{randoNumbo}</>;
+  };
+
+  const makeGrid = () => {
+    return <></>;
+  };
+
+  return (
+    <section className="text-[3rem] font-bold grid grid-cols-3 grid-rows-3">
+      {makeGrid()}
+    </section>
+  );
 };
