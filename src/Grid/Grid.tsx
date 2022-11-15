8e8a1e62b7e5eef6a159d12d3f1ee74f1216655c
armaBurton <armanarma@armaburton.com>
2022-11-15 12:06:30 -0800

    9x9 grid

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,37 +1,21 @@
 import { BuildGrid } from "./BuildGrid/BuildGrid";
 
 export const Grid = () => {
-  // const randoNumbo = () => {
-  //   const randoNumbo = Math.ceil(Math.random() * 9);
-
-  //   return <>{randoNumbo}</>;
-  // };
-
-  const makeGrid = () => {
-    // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    return (
-      <>
-        <BuildGrid />
-      </>
-      // <>
-      //   {arr.map((a) => {
-      //     return (
-      //       <div
-      //         className="w-[75px] h-[75px] bg-slate-300 flex justify-center items-center"
-      //         key={a}
-      //       >
-      //         {randoNumbo()}
-      //       </div>
-      //     );
-      //   })}
-      // </>
-    );
-  };
+  const tiles: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   return (
     <section className="text-[3rem] font-bold grid grid-cols-3 grid-rows-3 gap-3">
-      <BuildGrid />
+      {/* <section className="text-[3rem] font-bold grid grid-cols-3 grid-rows-3 gap-3"> */}
+      {tiles.map((t) => {
+        return (
+          <div
+            key={t}
+            className="text-[3rem] font-bold grid grid-cols-3 grid-rows-3 gap-1"
+          >
+            <BuildGrid />
+          </div>
+        );
+      })}
     </section>
   );
 };
